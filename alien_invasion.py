b9dd10605bf531c6ce215349c9e5f71ee76de07a
wenshijie <276656491@qq.com>
2020-09-19 21:39:19 +0800

    调整飞船的速度

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,7 +12,6 @@ import game_functions as gf
 
 
 def run_game():
-
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
     ai_settings = Settings()
@@ -20,11 +19,10 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # 创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # 开始游戏的主循环
     while True:
-
         # 监视键盘和鼠标事件
         gf.check_events(ship)
         # 更新飞船位置
--- ship.py
+++ ship.py
@@ -8,9 +8,10 @@ import pygame
 
 
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """初始化飞船并设置其初始位置"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # 加载飞船图像并获取其外界矩形
         self.image = pygame.image.load('images/ship.bmp')
@@ -19,6 +20,7 @@ class Ship():
         # 将每艘新飞船放在屏幕中间
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.center = float(self.rect.centerx)
         # 移动标志
         self.moving_right = False
         self.moving_left = False
@@ -26,9 +28,10 @@ class Ship():
     def update(self):
         """根据移动标志调整飞船位置"""
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
 
     def blitme(self):
         """在指定位置绘制飞船"""
