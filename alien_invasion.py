153f288c6a71438acb247b7bf3a3980768bba4f8
wenshijie <276656491@qq.com>
2020-09-19 21:15:25 +0800

    允许不断移动

---
+++ alien_invasion.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on =2020-09-14
+
+@author: wenshijie
+"""
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+
+    # 初始化游戏并创建一个屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # 创建一艘飞船
+    ship = Ship(screen)
+
+    # 开始游戏的主循环
+    while True:
+
+        # 监视键盘和鼠标事件
+        gf.check_events(ship)
+        # 更新飞船位置
+        ship.update()
+        # 每次循环都重绘屏幕并绘制
+        gf.update_screen(ai_settings, screen, ship)
+
+
+run_game()
---
+++ game_functions.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on =2020-09-15
+
+@author: wenshijie
+"""
+import sys
+import pygame
+
+
+def check_events(ship):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
+
+
+
+def update_screen(ai_settings, screen, ship):
+    """更新屏幕上的图像并切换到新屏幕"""
+    # 每次循环都重新绘制屏幕
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+    # 让最近绘制的屏幕可见
+    pygame.display.flip()
