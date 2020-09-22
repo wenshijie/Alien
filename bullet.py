1c54f5983296013ec9c1bc0f954514c21c26d1a8
wenshijie <276656491@qq.com>
2020-09-22 23:46:35 +0800

    上下左右移动

---
+++ bullet.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on =2020-09-22
+
+@author: wenshijie
+"""
--- settings.py
+++ settings.py
@@ -16,3 +16,8 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 1.5
+        # 子弹设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
