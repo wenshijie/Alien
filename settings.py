53a698ae9807a7ef885382f0e582f74b847c3954
wenshijie <276656491@qq.com>
2020-10-05 14:11:09 +0800

    创建Bullet类

--- settings.py
+++ settings.py
@@ -11,11 +11,13 @@ class Settings():
 
     def __init__(self):
         """初始化游戏的设置"""
+
         # 屏幕设置
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 1.5
+
         # 子弹设置
         self.bullet_speed_factor = 1
         self.bullet_width = 3
